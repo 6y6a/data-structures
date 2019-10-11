ca498386eb0c7d222cf94cc9d3b3a1643f77f90b
6y6a <gubinalexeymain@gmail.com>
2019-10-11 18:58:44 +0300

    start point of usage

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const LinkedList = require('./LinkedList')
+
+
+const a = 1, b = 2, c = 3, d = 4;
+
+const list = new LinkedList();
+
+list.addItem(a)
+list.addItem(b)
+list.addItem(c)
+list.addItem(a)
+list.addItem(b)
+list.addItem(c)
+
+list.showList();
+
+
+list.removeItem(d)
+
+console.log(list.length)
+
